fef09ee3b492bc5b947b9f07ce79757be1b225a7
Kai Toedter <kai@toedter.com>
2021-06-13 21:10:09 +0200

    chore: update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve("./src/index.ts"),
@@ -46,6 +47,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       inject: true,
       template: path.resolve("./src/index.html"),
